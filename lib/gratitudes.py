f97f7a82bae76fc5387d2489b532597523b94f7a
Benedict <bvaluks@gmail.com>
2023-07-24 16:14:16 +0100

    24/07 sixth commit

--- lib/gratitudes.py
+++ lib/gratitudes.py
@@ -11,3 +11,17 @@ class Gratitudes:
         formatted = "Be grateful for: "
         formatted += ", ".join(self.gratitudes)
         return formatted
+
+class Present:
+    def __init__(self):
+        self.contents = None
+
+    def wrap(self, contents):
+        if self.contents is not None:
+            raise Exception("A contents has already been wrapped.")
+        self.contents = contents
+
+    def unwrap(self):
+        if self.contents is None:
+            raise Exception("No contents have been wrapped.")
+        return self.contents
--- tests/test_present.py
+++ tests/test_present.py
@@ -5,3 +5,18 @@ def test_present_initial():
     example = Present()
     result = example.contents
     assert result == None
+
+def test_present_wrap():
+    example = Present()
+    example.wrap('Book')
+    with pytest.raises(Exception) as e:
+        example.wrap('Chocolate')
+    error_message = str(e.value)
+    assert error_message == "A contents has already been wrapped."
+
+def test_present_unwrap():
+    example = Present()
+    with pytest.raises(Exception) as e:
+        example.unwrap()
+    error_message = str(e.value)
+    assert error_message == "No contents have been wrapped."
