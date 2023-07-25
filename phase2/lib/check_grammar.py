0b2b217af3fe55cbb8e2825575a67b8e33651b32
ami <amiday22@gmail.com>
2023-07-25 16:07:53 +0100

    25-07-2023 Exercise 2 complete

--- phase2/lib/check_grammar.py
+++ phase2/lib/check_grammar.py
@@ -1,11 +1,14 @@
 def check_grammar(text):
-    if text == '':
-        return False
-    else:
-        if text[0].islower():
+    if isinstance(text,str):
+        if text == '':
             return False
         else:
-            if text[-1] != '!' and text[-1] != '.':
-                return False
+            if text[0].isupper():
+                if text[-1] == '!' or text[-1] == '.':
+                    return True
+                else:
+                    return False
             else:
-                return True
+                return False
+    else:
+        raise Exception("Error: input not a string")
--- phase2/tests/test_check_grammar.py
+++ phase2/tests/test_check_grammar.py
@@ -1,4 +1,5 @@
 from lib.check_grammar import *
+import pytest
 
 def test_empty_string_returns_false():
     result = check_grammar("")
@@ -8,11 +9,21 @@ def test_string_doesnt_start_with_lowercase():
     result = check_grammar('ami')
     assert result == False
 
-def test_string_does_start_with_uppercase():
-    result = check_grammar('Ami')
-    assert result == True
-
 def test_string_does_start_with_uppercase_and_doesnt_end_with_punctuation_mark():
     result = check_grammar('Ami')
     assert result == False
+
+def test_string_does_start_with_uppercase_and_does_end_with_punctuation_mark():
+    result = check_grammar('Makers!')
+    assert result == True
+
+def test_string_doesnt_start_with_uppercase_and_does_end_with_punctuation_mark():
+    result = check_grammar("makers!")
+    assert result == False
+
+def test_argument_not_string():
+    with  pytest.raises(Exception) as e:
+        check_grammar(100)
+    error_message = str(e.value)
+    assert error_message == "Error: input not a string"
 
