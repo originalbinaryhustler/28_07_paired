41d0c69ab3a4636f5a62c772e40806aab31d7b67
Benedict <bvaluks@gmail.com>
2023-07-26 16:48:42 +0100

    26/07 fourth commit - class test drive exercise

---
+++ phase2/tests/test_test_drive_a_class.py
@@ -0,0 +1,26 @@
+import pytest
+from lib.test_drive_a_class import *
+
+def test_initialise_title_assigned():
+    example = DiaryEntry('Diary', 'My name is Ben')
+    assert example.title == 'Diary'
+
+def test_initialise_contents_assigned():
+    example = DiaryEntry('Diary', 'My name is Ben')
+    assert example.contents == 'My name is Ben'
+
+def test_format_functions_correctly():
+    example = DiaryEntry('Diary', 'My name is Ben')
+    assert example.format() == 'My Diary: My name is Ben'
+
+def test_count_words_functions_correctly():
+    example = DiaryEntry('Diary', 'My name is Ben')
+    assert example.count_words() == 6
+
+def test_reading_time_functions_correctly():
+    example = DiaryEntry('Diary', 'My name is Ben')
+    assert example.reading_time(3) == 2
+
+def test_reading_chunk_intial_assert():
+    example = DiaryEntry('Diary', 'My name is Ben')
+    assert example.reading_chunk(3, 1) == 'My Diary: My'
